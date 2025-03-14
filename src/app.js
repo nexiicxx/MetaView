69b1491af3929dba26c70cfdc14475b8acb3fa59
Nexii <nexiicx@gmail.com>
2025-03-14 15:40:56 +0100

    Upload

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
+};
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
